888928ab7a1eb885fac82297d56175ddab50034c
Veerkumar Patil <veepatil@redhat.com>
2025-09-02 15:55:20 +0530

    Update shipment endpoint to accept an ID parameter and return detailed shipment information including price and currency

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
+from typing import Any
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
-@app.get("/shipment")
-def get_shipment():
+@app.get(f"/shipment/{id}")
+def get_shipment(id: int)->dict[str, Any]:
     return {
-        "Content": "wooden tabel",
-        "Status": "In transit"
+        "id": id,
+        "Content": "Wooden Table",
+        "Status": "In transit",
+        "Price": 1000,
+        "Currency": "USD"
     }
 
 @app.get("/scalar", include_in_schema=False)
